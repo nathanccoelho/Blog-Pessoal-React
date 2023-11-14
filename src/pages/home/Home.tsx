b8b914f25d9579a6e7020d290b8267c5bee6b65c
Nathan Gabriel <nathancoelho.job@gmail.com>
2023-11-14 11:56:41 -0300

    Class postagens completed

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,7 @@
+import ListaPostagens from "../../components/postagens/listaPostagens/ListaPostagens"
+import ModalPostagem from "../../components/postagens/modalPostagem/ModalPostagem"
 
 function Home() {
-
-    
-
     return (
         <>
             <div className="bg-indigo-900 flex justify-center">
@@ -16,10 +15,8 @@ function Home() {
                         </p>
 
                         <div className="flex justify-around gap-4">
-                            <div className='rounded text-white 
-                                            border-white border-solid border-2 py-2 px-4'
-                                >
-                                Nova Postagem
+                            <div className="flex justify-around gap-4">
+                                <ModalPostagem />
                             </div>
                         </div>
                     </div>
@@ -33,6 +30,8 @@ function Home() {
                     </div>
                 </div>
             </div>
+
+            <ListaPostagens />
         </>
     )
 }
