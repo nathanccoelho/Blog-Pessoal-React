b699f3ce89f71b91c0a0d318844543b012d394f1
Nathan Coelho <nathancoelho.job@gmail.com>
2023-11-16 14:52:58 -0300

    Update NavBar and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import DeletarTema from './components/temas/deletarTema/DeletarTema';
 import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens';
 import FormularioPostagem from './components/postagens/formularioPostagem/FormularioPostagem';
 import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import Perfil from './pages/perfil/Perfil';
 
 function App() {
     return (
@@ -32,6 +33,8 @@ function App() {
                         <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
                         <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
                         <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+                        <Route path="perfil" element={<Perfil/>} />
+
                     </Routes>
                 </div>
                 <Footer />
